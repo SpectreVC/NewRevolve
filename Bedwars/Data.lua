e379bd70fb8af94ad9f98eb71dc9b53f345eb1a9
NexusNV <154760436+SpectreVC@users.noreply.github.com>
2024-03-19 21:10:08 +0200

    Create Data.lua

---
+++ Bedwars/Data.lua
@@ -0,0 +1,12 @@
+local Data_Path = "NewRevolve/Saves/Data.json"
+
+local Data_Check = game:GetService("HttpService"):JSONDecode(readfile(Data_Path))
+
+if Data_Check then
+writefile(Data_Path, game:GetService("HttpService"):JSONEncode(false))
+  loadfile("NewRevolve/Config/MainSrc.lua")()
+end
+
+if not Data_Check then
+loadfile("NewRevolve/Config/MainSrc.lua")()
+end

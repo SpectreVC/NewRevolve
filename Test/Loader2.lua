78272f83dcab10697213f3256d4153524eb5836f
NexusNV <154760436+SpectreVC@users.noreply.github.com>
2024-03-19 19:59:27 +0200

    Update Loader2.lua

--- Test/Loader2.lua
+++ Test/Loader2.lua
@@ -4,6 +4,8 @@ local Data_Check = game:GetService("HttpService"):JSONDecode(readfile(Data_Path)
 
 if Data_Check then
 writefile(Data_Path, game:GetService("HttpService"):JSONEncode(false))
+  task.wait(3)
+  loadfile("NewRevolve/Config/MainSrc.lua")()
 end
 
 if not Data_Check then

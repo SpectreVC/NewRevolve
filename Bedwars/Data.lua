f2a6a5cb1238abd66392fc09e6bc1161d67b9143
NexusNV <154760436+SpectreVC@users.noreply.github.com>
2024-03-19 21:21:55 +0200

    Update Data.lua

--- Bedwars/Data.lua
+++ Bedwars/Data.lua
@@ -4,7 +4,7 @@ local Data_Check = game:GetService("HttpService"):JSONDecode(readfile(Data_Path)
 
 if Data_Check then
 writefile(Data_Path, game:GetService("HttpService"):JSONEncode(false))
-  task.wait(3)
+  task.wait(0.3)
   loadfile("NewRevolve/Config/MainSrc.lua")()
 end
 
